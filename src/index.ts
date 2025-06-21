8007ef2771ddccbb198a0d87623913bc3cb9ac8c
Andrei <andrei@MacBook-Pro-Andrei.local>
2025-06-21 14:06:43 +0300

    fix: minor deployment fix

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const main = async () => {
     const fastify = await configureServer();
 
     const address = await fastify.listen({
-        port: fastify.config.RUNNING_PORT,
+        port: fastify.config.PORT,
         host: fastify.config.HOST,
     });
 
